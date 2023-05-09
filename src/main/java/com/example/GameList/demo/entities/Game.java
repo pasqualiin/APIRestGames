a102ad0e53a12ae98c8dbf8b91961f4ee9cfa3a1
Guilherme Pasqualin <guilhermepasqualin@gmail.com>
2023-05-09 11:16:09 -0300

    Game seed

--- src/main/java/com/example/GameList/demo/entities/Game.java
+++ src/main/java/com/example/GameList/demo/entities/Game.java
@@ -19,10 +19,12 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
@@ -34,7 +36,7 @@ public class Game {
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataforms = plataforms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -74,11 +76,11 @@ public class Game {
 	}
 
 	public String getPlataforms() {
-		return plataforms;
+		return platforms;
 	}
 
-	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+	public void setPlataforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
