1ea7f4020d71c721a2cfad020d8d1d6d540bb669
Guilherme Pasqualin <guilhermepasqualin@gmail.com>
2023-05-09 10:42:23 -0300

    Project created

---
+++ src/test/java/com/example/GameList/demo/GameListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameList.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
