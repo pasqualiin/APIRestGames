013813a005cd64f4a697dab67793fa34b4c12a5d
Guilherme Pasqualin <guilhermepasqualin@gmail.com>
2023-05-12 22:48:28 -0300

    Homologacao ok

--- src/main/java/com/example/GameList/demo/projections/GameMinProjection.java
+++ src/main/java/com/example/GameList/demo/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.example.GameList.demo.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
